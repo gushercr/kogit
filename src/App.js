4297bfed82af8e57a6533c8e28cd5d307a9453c6
sofi1813 <64979363+sofi1813@users.noreply.github.com>
2021-08-02 22:20:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import Header from'./componentes/header';
 import Footer from'./componentes/footer';
 import PageNotFound from './vistas/PageNotFound';
 import recuperarPassword from './vistas/recuperarPassword';
-import Publicaciones from '../src/vistas/publicaciones'
+import RecuperarPasswordEmail from './vistas/recuperarPasswordEmail';
+
+import Repositorio from'./vistas/publicaciones';
+import Publicaciones from'./vistas/repositorio';
 import NPost from '../src/vistas/nuevoPost'
 import Post from './vistas/Post';
 import ResultBusqueda from './vistas/resultBusquda';
-import RecuperarPasswordEmail from './vistas/recuperarPasswordEmail';
+
 // import name from './vistas/resultBusquda';
 import './css/styles.css';
 import {
@@ -19,10 +22,6 @@ import {
   Route
 } from "react-router-dom";
 import PerfilUsuarioE from './vistas/PerfilUsuarioE';
-
-
-
-
 
 export default function App() {
   return (
@@ -36,6 +35,7 @@ export default function App() {
           <Route path="/Perfil" component={Perfil}/>
           <Route path="/Recuperar?key=:key" component={RecuperarPasswordEmail}/>
           <Route path="/Recuperar" component={recuperarPassword}/>
+          <Route path="/Repositorio" component={Repositorio}/>
           <Route path="/Publicaciones" component={Publicaciones}/>
           <Route path="/Post" component={Post}/>
           <Route path="/NuevoPost" component={NPost}/>
